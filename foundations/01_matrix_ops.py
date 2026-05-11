581ab9c83343d2777782abba223c4e80b91d9162
arjunsreelatha <arjunsreelatha070@gmail.com>
2026-05-11 11:32:42 +0530

    Add basic matrix operations and matrix multiplication

--- foundations/01_matrix_ops.py
+++ foundations/01_matrix_ops.py
@@ -5,6 +5,7 @@ A=np.array([[1,2,3],
 B = np.array([[7,8],
              [9,8],
              [11,12]])
+#shape of matrix(rows,coloumn)
 print("Shape of A:", A.shape)
 print("Shape of B:", B.shape)
 
@@ -13,9 +14,11 @@ C = np.array([[1,2],
 
 D = np.array([[4,6],
               [7,8]])
-
+#matric addition
 print("\nC + D =\n", C+D)
-
+#scalar multiplication
 print("\n3 * C= \n", 3*C)
-
+#matrix element wise #multiplication
 print("\nC * D = \n", C*D)
+#matrix multiplication
+print("\nA @ B = \n", A@B)
